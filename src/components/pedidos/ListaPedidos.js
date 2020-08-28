6c4fedf4e8c6953d6baf5fbe988932ea8c952b2a
unknown <judithportocarrero99@gmail.com>
2020-08-28 01:33:04 -0500

    estilos scss

--- src/components/pedidos/ListaPedidos.js
+++ src/components/pedidos/ListaPedidos.js
@@ -3,29 +3,6 @@ import DetallePedido from "./DetallePedido";
 import {productoServido} from '../../firebase/firestore'
 import './ListaPedidos.scss';
 
-const formatoFecha = (time) => {
-  let date = new Date(time);
-  return date.toLocaleString();
-}
-
-const intervaloTiempo = (date1,date2) => {
-  let date1_ms = date1.getTime();
-  let date2_ms = date2.getTime();
-
-  // Calculate the difference in milliseconds
-  let difference_ms = date2_ms - date1_ms;
-  //take out milliseconds
-  difference_ms = difference_ms/1000;
-  let seconds = Math.floor(difference_ms % 60);
-  difference_ms = difference_ms/60; 
-  let minutes = Math.floor(difference_ms % 60);
-  difference_ms = difference_ms/60; 
-  let hours = Math.floor(difference_ms % 24);  
-  // let days = Math.floor(difference_ms/24);
-  
-  return  hours + ' H, ' + minutes + ' M '+ seconds + ' S';
-}
-
 const ListaPedidos = ({ pedidos, esHistorico }) => {
     console.log(esHistorico);
     return (
