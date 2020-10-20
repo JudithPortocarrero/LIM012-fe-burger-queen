749743407b6e5f59d82da61d1f3fd198fe964dfc
unknown <judithportocarrero99@gmail.com>
2020-10-20 15:30:58 -0500

    Updates

---
+++ precache-manifest.0a667cb566fdf58f40a19a5182fd3a56.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b573f8d8aaa03abddf0307cee486fc",
+    "url": "/LIM012-fe-burger-queen/index.html"
+  },
+  {
+    "revision": "2829a3f8f1ce6f023c8b",
+    "url": "/LIM012-fe-burger-queen/static/css/main.ae2cadf3.chunk.css"
+  },
+  {
+    "revision": "6f6428471355687baf79",
+    "url": "/LIM012-fe-burger-queen/static/js/2.5411b868.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/LIM012-fe-burger-queen/static/js/2.5411b868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2829a3f8f1ce6f023c8b",
+    "url": "/LIM012-fe-burger-queen/static/js/main.78eeab68.chunk.js"
+  },
+  {
+    "revision": "5257e240aaaa0a20b3c9",
+    "url": "/LIM012-fe-burger-queen/static/js/runtime-main.9e1cd89d.js"
+  },
+  {
+    "revision": "aa0f216b142c6b175496f0dbda3db9b6",
+    "url": "/LIM012-fe-burger-queen/static/media/fondoladrillo.aa0f216b.jpg"
+  },
+  {
+    "revision": "2cf2820ae24c9792bb435e1c89954a84",
+    "url": "/LIM012-fe-burger-queen/static/media/logoBG.2cf2820a.png"
+  }
+]);
